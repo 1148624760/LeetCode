dbb2b0939fb52064b1beb5e4f12c6cf34e1102a0
Channing <1148624760@qq.com>
2021-04-26 20:52:19 +0800

    offer_55-2

---
+++ 剑指offer/offer_55-2.cpp
@@ -0,0 +1,24 @@
+#include "head.h"
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    int maxDepth(TreeNode* root) {
+        if (!root) return 0;
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+
+public:
+    bool isBalanced(TreeNode* root) {
+        if (!root) return true;
+        return abs(maxDepth(root->left) - maxDepth(root->right)) <= 1 && isBalanced(root->left) && isBalanced(root->right);
+    }
+};
